2416d507296ceb137a5a7ed3246017687be84a37
wingyou <winwing.you@gmail.com>
2026-07-04 13:33:55 +0900

    Config rstml for Treesj

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,46 +1,64 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate',
-  branch = 'master',
-  config = function ()
-    require'nvim-treesitter.configs'.setup {
-      -- install_dir = vim.fn.stdpath('data') .. '/site',
-      modules = {},
-      ensure_installed = {
-        'c',
-        'lua',
-        'vim',
-        'vimdoc',
-        'markdown',
-        'markdown_inline',
-        'java',
-        'javascript',
-        'typescript',
-        'html',
-      },
-      ignore_install = {},
-      auto_install = true,
-      sync_install = false,
-      highlight = {
-        enable = true,
-        -- additional_vim_regex_highlighting = true,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "M",
-          node_incremental = "M",
-          scope_incremental = "H",
-          node_decremental = "L",
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
+    branch = 'master',
+    config = function ()
+      require'nvim-treesitter.configs'.setup {
+        -- install_dir = vim.fn.stdpath('data') .. '/site',
+        modules = {},
+        ensure_installed = {
+          'c',
+          'lua',
+          'vim',
+          'vimdoc',
+          'markdown',
+          'markdown_inline',
+          'java',
+          'javascript',
+          'typescript',
+          'html',
+          'rust',
+        },
+        ignore_install = {},
+        auto_install = true,
+        sync_install = false,
+        highlight = {
+          enable = true,
+          -- additional_vim_regex_highlighting = true,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "M",
+            node_incremental = "M",
+            scope_incremental = "H",
+            node_decremental = "L",
+          }
+        },
+        indent = {
+          enable = true
         }
-      },
-      indent = {
-        enable = true
       }
-    }
 
-  end
+    end
+  },
+  {
+    "rayliwell/tree-sitter-rstml",
+    dependencies = { "nvim-treesitter" },
+    build = ":TSUpdate",
+    config = function ()
+      require("tree-sitter-rstml").setup()
+    end
+  },
+  -- Automatic tag closing and renaming (optional but highly recommended)
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
 }
 -- return {
 --   'nvim-treesitter/nvim-treesitter',
--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -3,8 +3,121 @@ return {
   keys = { 'gj' },
   dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
   config = function()
+    local lang_utils = require('treesj.langs.utils')
+
+    local rstml_config = {
+      field_declaration_list = lang_utils.set_preset_for_dict(),
+      declaration_list = lang_utils.set_preset_for_statement({
+        join = {
+          force_insert = '',
+        },
+      }),
+      field_initializer_list = lang_utils.set_preset_for_dict(),
+      struct_pattern = lang_utils.set_preset_for_dict({
+        both = {
+          omit = { '{' },
+        },
+      }),
+      parameters = lang_utils.set_preset_for_args({
+        split = {
+          last_separator = true,
+        },
+      }),
+      arguments = lang_utils.set_preset_for_args({
+        split = {
+          last_separator = true,
+        },
+      }),
+      tuple_type = lang_utils.set_preset_for_args({
+        split = {
+          last_separator = true,
+        },
+      }),
+      enum_variant_list = lang_utils.set_preset_for_list(),
+      tuple_expression = lang_utils.set_preset_for_args({
+        split = {
+          last_separator = true,
+        },
+      }),
+      block = lang_utils.set_preset_for_statement({
+        join = {
+          no_insert_if = {
+            lang_utils.helpers.if_penultimate,
+            'function_item',
+          },
+          format_tree = function(tsj)
+            local node = tsj:tsnode()
+            local parents = { 'match_arm', 'closure_expression' }
+            local has_parent = vim.tbl_contains(parents, node:parent():type())
+            if has_parent and node:named_child_count() == 1 then
+              tsj:remove_child({ '{', '}' })
+            end
+          end,
+        },
+      }),
+      value = lang_utils.set_preset_for_statement({
+        split = {
+          format_tree = function(tsj)
+            if tsj:type() ~= 'block' then
+              tsj:wrap({ left = '{', right = '}' })
+            end
+          end,
+        },
+        join = {
+          no_insert_if = { lang_utils.helpers.if_penultimate },
+          format_tree = function(tsj)
+            local node = tsj:tsnode()
+            local parents = { 'match_arm', 'closure_expression' }
+            local has_parent = vim.tbl_contains(parents, node:parent():type())
+            if has_parent and node:named_child_count() == 1 then
+              tsj:remove_child({ '{', '}' })
+            end
+          end,
+        },
+      }),
+      match_arm = {
+        target_nodes = { 'value' },
+      },
+      closure_expression = {
+        target_nodes = { ['body'] = 'value' },
+      },
+      use_list = lang_utils.set_preset_for_list(),
+      array_expression = lang_utils.set_preset_for_list(),
+      parenthesized_expression = lang_utils.set_preset_for_args(),
+      let_declaration = {
+        target_nodes = {
+          'field_declaration_list',
+          'field_initializer_list',
+          'array_expression',
+        },
+      },
+      function_item = {
+        target_nodes = {
+          'parameters',
+        },
+      },
+      enum_item = {
+        target_nodes = {
+          'enum_variant_list',
+        },
+      },
+      use_declaration = {
+        target_nodes = {
+          'use_list',
+        },
+      },
+      trait_item = {
+        target_nodes = {
+          'declaration_list',
+        },
+      },
+    }
+
     require('treesj').setup {
-      use_default_keymaps = false
+      use_default_keymaps = false,
+      langs = {
+        rust_with_rstml = rstml_config
+      }
     }
 
     vim.keymap.set('n', 'gj', require'treesj'.toggle, { desc = '[Edit] toggle treesj' })
